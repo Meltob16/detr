7e05b39f124886a21839f5f96baaf5c7582d736e
Tob <tobias.mellum@gmail.com>
2025-02-04 10:09:11 +0100

    fix version clause

--- util/misc.py
+++ util/misc.py
@@ -18,7 +18,9 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.7:
+from packaging.version import Version
+# if float(torchvision.__version__[:3]) < 0.7:
+if Version(torchvision.__version__) < Version('0.7.0'):
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
@@ -454,7 +456,8 @@ def interpolate(input, size=None, scale_factor=None, mode="nearest", align_corne
     This will eventually be supported natively by PyTorch, and this
     class can go away.
     """
-    if float(torchvision.__version__[:3]) < 0.7:
+    # if float(torchvision.__version__[:3]) < 0.7:
+    if Version(torchvision.__version__) < Version('0.7.0'):
         if input.numel() > 0:
             return torch.nn.functional.interpolate(
                 input, size, scale_factor, mode, align_corners
